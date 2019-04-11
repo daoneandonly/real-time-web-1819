f5ba5e052cb038e8d72798fa4623e26014ec9fc5
Steven Whitton <stevenawhitton@gmail.com>
2019-04-11 14:13:55 +0200

    Added usernames and assigning colors

--- app.js
+++ app.js
@@ -4,11 +4,41 @@ const path = require('path')
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
+const myData = {
+  count: 0,
+  colors: {
+    0: {
+      name: 'orange',
+      value: '#f35f05'
+    },
+    1: {
+      name: 'red',
+      value: '#FF0000'
+    },
+    2: {
+      name: 'magenta',
+      value: '#FF00FF'
+    },
+    3: {
+      name: 'steelblue',
+      value: '#4682B4'
+    }
+  }
+}
+
 app.use(express.static(path.join(__dirname, 'public/draw')))
 
 io.on('connection', function(socket) {
-  socket.on('change color', function(location) {
-    io.emit('change color', location)
+  myData.count++
+  io.emit('active users', myData.count)
+  io.emit('assign color', myData.colors[Math.floor(Math.random() * 4)])
+  socket.on('change color', function(location, color) {
+    // save data here
+    io.emit('change color', location, color)
+  })
+  socket.on('disconnect', () => {
+    myData.count = myData.count - 1
+    io.emit('active users', myData.count)
   })
 })
 
--- public/draw/src/js/script.js
+++ public/draw/src/js/script.js
@@ -1,27 +1,52 @@
-var socket = io()
-var picture = document.querySelector('.picture')
-var gridSize = 24
-var currentColor = document.querySelector('input[type=color]')
-var colorValue = currentColor.value
+const socket = io()
+const picture = document.querySelector('.picture')
+const gridSize = 24
+const defaultColor = '#000000'
+const colorValue = { value: defaultColor }
+const userName = document.querySelector('.overlay input[type=text]').focus()
 
-currentColor.addEventListener('change', () => {
-  colorValue = currentColor.value
+const nameForm = document.querySelector('.overlay form')
+
+nameForm.addEventListener('submit', e => {
+  e.preventDefault()
+  let nameElement = document.querySelector('.overlay input[type=text]')
+  let name = nameElement.value
+  console.log(name)
+  nameElement.value = ''
+  let overlay = document.querySelector('.overlay')
+  overlay.parentNode.removeChild(overlay)
+})
+
+socket.on('assign color', colorData => {
+  console.log(colorData)
+  console.log(
+    'You got assigned the color ' +
+      "'" +
+      colorData.name +
+      "'" +
+      ' with hexcode ' +
+      colorData.value
+  )
+  colorValue.value = colorData.value
+  let teamName = document.createElement('p')
+  teamName.style = 'color: ' + colorData.value
+  teamName.textContent = 'You are in team ' + colorData.name
+  document.querySelector('aside').appendChild(teamName)
 })
 
 function changeColor(e) {
   let block = e.target
-  block.style = 'background-color: ' + colorValue + ';'
-  socket.emit('change color', block.classList.value)
+  block.style = 'background-color: ' + colorValue.value + ';'
+  socket.emit('change color', block.classList.value, colorValue.value)
 }
 
-for (var i = 0; i < gridSize; i++) {
+for (let i = 0; i < gridSize; i++) {
   let row = document.createElement('div')
   row.classList.add('row' + i)
-  for (var ii = 0; ii < gridSize; ii++) {
+  for (let ii = 0; ii < gridSize; ii++) {
     let cell = document.createElement('div')
     cell.classList.add('row' + i + 'cell' + ii)
     cell.addEventListener('click', e => {
-      console.log(e)
       changeColor(e)
     })
     row.appendChild(cell)
@@ -29,8 +54,12 @@ for (var i = 0; i < gridSize; i++) {
   picture.appendChild(row)
 }
 
-socket.on('change color', function(location) {
-  console.log('New Cell to be colored ' + location)
+socket.on('change color', function(location, color) {
   let colorCell = document.querySelector('.' + location)
-  colorCell.style = 'background-color: black;'
+  colorCell.style = 'background-color: ' + color
+})
+
+socket.on('active users', users => {
+  let userCount = document.querySelector('.count')
+  userCount.textContent = users
 })
