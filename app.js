7d2b914ec53dbcf32fcd5ab5a26b40781645a0f3
Steven Whitton <stevenawhitton@gmail.com>
2019-05-17 22:47:44 +0200

    Added styling and improved navigation

--- app.js
+++ app.js
@@ -7,19 +7,39 @@ const axios = require('axios')
 const header = {
   headers: { 'Client-ID': 'z79wro6y3mydfh153v7fnaf2kquc5z' }
 }
+const streamData = {}
 
 app.use(express.static(path.join(__dirname, 'public/stream')))
 
+axios
+  .get('https://api.twitch.tv/helix/games/top?first=20', header)
+  .then(res => {
+    streamData.topGames = res.data.data
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
 io.on('connection', function(socket) {
-  console.log('a user has connected.')
-  axios
-    .get('https://api.twitch.tv/helix/streams?game_id=32399&first=4', header)
-    .then(res => {
-      socket.emit('data', res.data)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+  console.log('New connection established.')
+
+  socket.emit('renderHome', streamData.topGames)
+
+  socket.on('newList', game => {
+    axios
+      .get(
+        'https://api.twitch.tv/helix/streams?game_id=' + game.id + '&first=12',
+        header
+      )
+      .then(res => {
+        streamData.data = res.data.data
+        streamData.currentGame = game.name
+        socket.emit('streamerList', streamData)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
 })
 
-server.listen(3000)
+server.listen(404)
--- public/stream/src/js/script.js
+++ public/stream/src/js/script.js
@@ -1,18 +1,60 @@
 const socket = io()
+const app = document.querySelector('.app')
+const data = {}
 
-socket.on('connection', () => {
-  console.log('Hoi!')
+function renderHome(topGames) {
+  data.topGames = topGames
+  topGames.forEach(game => {
+    let section = document.createElement('section')
+    let image = game.box_art_url.replace('{width}x{height}', '480x638')
+    section.classList.add('gamecover')
+    section.style = 'background-image: url(' + image + ');'
+    section.innerHTML = `<a href="#"><h1> ${game.name} </h1></a>`
+    section.addEventListener('click', e => {
+      e.preventDefault()
+      window.location.hash = game.id
+      socket.emit('newList', { id: game.id, name: game.name })
+    })
+    app.appendChild(section)
+  })
+}
+
+function createTiles(streamData) {
+  streamData.data.forEach(stream => {
+    let section = document.createElement('section')
+    let image = stream.thumbnail_url.replace('{width}x{height}', '480x272')
+    section.classList.add('tile')
+    section.style = 'background-image: url(' + image + ');'
+    section.innerHTML = `<a target=_blank href="https://twitch.tv/${
+      stream.user_name
+    }"><h2> ${stream.user_name} </h2>
+    <p> Viewers: ${stream.viewer_count} </p></a>`
+    app.appendChild(section)
+  })
+}
+socket.on('renderHome', topGames => {
+  renderHome(topGames)
 })
 
-socket.on('data', function(data) {
-  data.data.forEach(stream => {
-    let section = document.createElement('section')
-    let body = document.querySelector('body')
-    let image = stream.thumbnail_url.replace('{width}x{height}', '480x272')
-    section.style = 'background-image: url(' + image + ');'
-    section.innerHTML = `<h2> ${stream.user_name} </h2>
-    <p> Viewers: ${stream.viewer_count} </p>`
-    body.appendChild(section)
+socket.on('streamerList', function(streamData) {
+  document.querySelectorAll('.app > section').forEach(tile => {
+    tile.parentNode.removeChild(tile)
   })
-  console.log(JSON.stringify(data.data))
+  createTiles(streamData)
+  let back = document.createElement('button')
+  back.textContent = 'Back to overview'
+  back.addEventListener('click', () => {
+    window.location.hash = 'home'
+  })
+  app.insertBefore(back, app.children[0])
+})
+
+window.addEventListener('hashchange', () => {
+  window.scrollTop = 0
+  if (window.location.hash == '#home') {
+    app.innerHTML = ''
+    renderHome(data.topGames)
+  } else {
+    //redner
+  }
 })
